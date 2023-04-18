1a94494baa369194b33c8f6702e86dc7c8f133f4
Dennis Mwea <mweadennis2@gmail.com>
2023-04-18 17:29:27 +0300

    Code refactoe

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
+    public function welcome()
+    {
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+
     public function dashboard()
     {
         return Inertia::render('Dashboard');
