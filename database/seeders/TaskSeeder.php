3605804bf055297f23c6444ba380f16b693a7802
Dennis Mwea <mweadennis2@gmail.com>
2023-04-18 17:23:00 +0300

    Add some sample tasks

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use App\Models\User;
+use App\Models\UserTask;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        User::all()->each(function (User $user) {
+            Task::factory()->count(20)->has(UserTask::factory()->state(fn(array $attrs, Task $task) => [
+                'user_id' => $user->id,
+                'status_id' => $task->status_id,
+            ]), 'task')->create();
+        });
+    }
+}
