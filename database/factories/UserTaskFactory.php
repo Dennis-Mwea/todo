414e3ae3636b9daa492bed28153716d2757397dd
Dennis Mwea <mweadennis2@gmail.com>
2023-04-18 17:20:11 +0300

    Fix bug

--- database/factories/UserTaskFactory.php
+++ database/factories/UserTaskFactory.php
@@ -23,13 +23,13 @@ class UserTaskFactory extends Factory
         $start = $this->faker->dateTime;
 
         return [
-            'user_id' => User::all()->random()->id,
-            'task_id' => Task::all()->random()->id,
-            'due_date' => $this->faker->dateTime,
             'start_date' => $start,
-            'end_date' => $this->faker->dateTimeBetween($start),
-            'remarks' => $this->faker->sentences(asText: true),
+            'remarks' => $this->faker->sentence,
+            'due_date' => $this->faker->dateTime,
+            'task_id' => Task::all()->random()->id,
+            'user_id' => User::all()->random()->id,
             'status_id' => Status::all()->random()->id,
+            'end_date' => $this->faker->dateTimeBetween($start),
         ];
     }
 }
