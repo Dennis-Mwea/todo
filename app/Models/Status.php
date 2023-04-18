17897e181b9dbb2d89497d1d34be86f68405189d
Dennis Mwea <mweadennis2@gmail.com>
2023-04-18 16:48:43 +0300

    Add tasks db setup

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Status extends Model
@@ -18,4 +19,14 @@ class Status extends Model
     protected $fillable = [
         'name',
     ];
+
+    /**
+     * Get all the tasks in this status instance.
+     *
+     * @return HasMany
+     */
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
---
+++ app/Models/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name', 'description', 'due_date', 'status_id',
+    ];
+
+    /**
+     * Get the status which the task is in.
+     *
+     * @return BelongsTo
+     */
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(Status::class);
+    }
+}
