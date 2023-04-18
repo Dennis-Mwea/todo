e634d73688eed445c0b0112e4fa7eb54d4c994e1
Dennis Mwea <mweadennis2@gmail.com>
2023-04-18 16:41:25 +0300

    Add statuses

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Status;
+use Illuminate\Database\Seeder;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        collect([
+            'Backlog', 'To Do', 'In Progress', 'In Review', 'Done',
+        ])->each(fn (string $status) => Status::updateOrCreate(['name' => $status]));
+    }
+}
