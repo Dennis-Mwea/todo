b4ea3360ba80b88a2f55609db2eacda96f7e7e0f
Dennis Mwea <mweadennis2@gmail.com>
2023-04-21 01:26:00 +0300

    Change font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Mulish', ...defaultTheme.fontFamily.sans],
             },
         },
     },
