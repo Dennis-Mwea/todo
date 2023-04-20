444afdc677f0a78bb38e6e68c54fedb23e917fd0
Dennis Mwea <mweadennis2@gmail.com>
2023-04-21 02:22:13 +0300

    Home page design

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,20 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class DashboardController extends Controller
 {
-    public function welcome()
+    public function welcome(): Response
     {
-        return Inertia::render('Welcome', [
-            'phpVersion' => PHP_VERSION,
-            'canLogin' => Route::has('login'),
-            'laravelVersion' => Application::VERSION,
-            'canRegister' => Route::has('register'),
-        ]);
+        return Inertia::render('Welcome');
     }
 
-    public function dashboard()
+    public function dashboard(): Response
     {
         return Inertia::render('Dashboard');
     }
---
+++ resources/js/Assets/Images/Next.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg fill="none" height="25" viewBox="0 0 24 25" width="24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M17.25 8.75L21 12.5M21 12.5L17.25 16.25M21 12.5H3" stroke="currentColor" stroke-linecap="round"
+              stroke-linejoin="round" stroke-width="1.5"/>
+    </svg>
+</template>
